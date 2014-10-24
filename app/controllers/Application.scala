890a8662f36e5cd52aa676f3d4520b3f12b4c48e
tbrown1979 <tbrown@uni.edu>
2014-10-24 18:11:17 -0500

    Adding test route and actor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,19 @@
 package controllers
 
+import actors._
+import play.api.Play.current
 import play.api._
+import play.api.libs.json._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def socket = WebSocket.acceptWithActor[JsValue, JsValue] {
+    request => out => PollSocketActor.props(out)
   }
 
 }
--- app/controllers/WebSocket.scala
+++ app/controllers/WebSocket.scala
@@ -5,6 +5,7 @@ import akka.actor._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.iteratee.{Concurrent, Iteratee}
 import play.api.libs.ws.WS
+import play.api.libs.json._
 import play.api.libs.json.{JsValue, Json}
 import play.api.Logger
 //import org.joda.time.DateTime
@@ -15,6 +16,6 @@ object PollSocketActor {
 
 class PollSocketActor(out: ActorRef) extends Actor {
   def receive = {
-    case msg: String => out ! s"I received your message: $msg"
+    case msg: JsValue => out ! s"I received your message: $msg"
   }
 }
