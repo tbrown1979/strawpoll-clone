e00fe57c472182d5f0e8909420240a037e26dfb6
tbrown1979 <tbrown@uni.edu>
2014-10-31 16:58:27 -0500

    Can now create a poll using Redis.

--- app/persistence/PollRepository.scala
+++ app/persistence/PollRepository.scala
@@ -1,47 +1,37 @@
 package persistence
 
+import util._
 import actors._
+import akka.actor.ActorSystem
 import akka.actor._
 import play.api.Play.current
 import play.api._
+import play.api.libs.concurrent.Akka
 import play.api.libs.json._
 import play.api.mvc._
+import scala.util.{Success, Failure}
 import scredis._
-import akka.actor.ActorSystem
-import play.api.libs.concurrent.Akka
+import scredis.serialization._
+import scredis.serialization.Implicits._
 
-// trait Repository[T] {
-//   def get(id: String): Option[T]
-//   def update(id: String)
-// }
+case class Poll(title: String, id: Option[Long], answers: Map[String,Int])
 
-// trait PollRepository {
-//   def get(pollId: String): Option[Poll]
+trait PollRepository extends FutureProvider {
+  def create(poll: Poll): Future[Poll]
+}
 
-// }
-
-trait RedisPollRepository {
-  val redis = Redis()
-  def addPoll
-
-
+trait RedisPollRepository extends PollRepository with PollFutureProvider {
   val redis = Redis()
   import redis.dispatcher
-  val timeout = 5 seconds
 
-  val test = redis.incr("pollNumber")
+  def create(poll: Poll): Future[Poll] = {
+    val pollId = redis.incr("pollId")
 
-  val plest: Future[Option[Map[String,String]]] = for {
-    n <- test
-    u <- test.map(n => redis.hmSet(s"poll:$n", Map("1" -> 4, "2" -> 2)))
-    g <- {println("running after"); redis.hGetAll(s"poll:$n")}
-  } yield g
-
-
-  plest onComplete {
-    case Success(c) =>
-      println(c)
-    //redis.quit()
-    case _ => println("broke")
+    for {
+      id <- pollId
+      unit <- redis.hmSet[Int](s"poll:$id", poll.answers)
+      answers <- redis.hGetAll[Int](s"poll:$id")
+    } yield Poll(poll.title, Some(id), answers.getOrElse(poll.answers))
   }
+
 }
