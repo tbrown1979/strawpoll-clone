21626a886b7f364baed9a4a41668a28f92b46178
tbrown1979 <tbrown@uni.edu>
2014-12-08 15:22:16 -0600

    Fixing some problems with the initial javascript. You can now vote

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -3,10 +3,10 @@ $(function() {
     console.log("Handler called");
     event.preventDefault();
 
-    var index = $("input[name=rate]:checked").val()
+    var index = $("input[name=option]:checked").val()
     var url = window.location.pathname;
     var id = url.substring(url.lastIndexOf('/') + 1);
-    var strData = JSON.stringify({"pollId": id, "index": index});
+    var strData = JSON.stringify({"pollId": id, "index": parseInt(index)});
     console.log(strData);
     $.ajax({
       type: "POST",
