d0258569f0f35f9df88614d7dfcb624448e7e69c
tbrown1979 <tbrown@uni.edu>
2015-01-28 14:27:28 -0600

    Now adds a new textfield if you click on the last textfield. This dynamically adds more fields for you if you need more. Basically just like strawpoll.me

--- public/javascripts/pollCreation.js
+++ public/javascripts/pollCreation.js
@@ -1,8 +1,33 @@
 $(function() {
-  $("#createPoll").submit(function( event ) {
+
+  function getOptions(asVal) {
     var options = $("div.optionDiv input")
-      .map(function(i, e) {return $(e).val();});
+    if (asVal) {
+      return options.map(function(i, e) {return $(e).val();});
+    }
+    return options;
+  }
+
+  function lastOption() {
+    var options = getOptions(false);
+    return options[options.length-1]
+  }
+
+  $("#createPoll").submit(function( event ) {
+    var options = getOptions(true);
     console.log(options);
     event.preventDefault();
   });
+
+  function addAnotherTextfield() {
+    var options = getOptions(false);
+    $(lastOption()).unbind("click");
+    $(lastOption()).after(
+      "<br><input type='text' name='option" + options.length + "'>"
+    );
+    $(lastOption()).click(addAnotherTextfield);
+  }
+
+  $(lastOption()).click(addAnotherTextfield);
+
 })
