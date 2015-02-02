351a5be653f7e858db32fee612ac9e20b21d6fff
tbrown1979 <tbrown@uni.edu>
2015-02-02 17:22:19 -0600

    Doing my best to make this look just like the actual strawpoll website.

--- public/javascripts/pollCreation.js
+++ public/javascripts/pollCreation.js
@@ -1,10 +1,11 @@
 $(function() {
 
   function getOptions(asVal) {
-    var options = $("div.optionDiv input")
+    var options = $("div.pollBody .pollOption input")
     if (asVal) {
       return options.map(function(i, e) {return $(e).val();});
     }
+    console.log(options);
     return options;
   }
 
@@ -21,16 +22,19 @@ $(function() {
   function addAnotherTextfield() {
     var options = getOptions(false);
     $(lastOption()).unbind("keyup");
-    $(lastOption()).after(
-      "<br><input type='text' name='option" + options.length + "'>"
+    $("div.pollBody").append(
+        "<div class='pollOption'> \
+          <span class='optionNumber'>" + (options.length+1) + ".</span> \
+          <input type='text' name='option" + (options.length+1) + "' placeholder='Enter poll option...'> \
+        </div>"
     );
     $(lastOption()).keyup(addAnotherTextfield);
   }
 
   $(lastOption()).keyup(addAnotherTextfield);
 
-  $("#createPoll").submit(function(event) {
-    event.preventDefault();
+  $(".createPoll").click(function(event) {
+    //event.preventDefault();
     console.log("Handler working!");
     var title = $(".title").val();
     var options = _.filter(getOptions(true), function(e) { return e; });
