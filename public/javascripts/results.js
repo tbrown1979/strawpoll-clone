97ff9ff64587abe68f72f5cfbd42f6c5a647153f
tbrown1979 <tbrown@uni.edu>
2015-02-04 17:49:47 -0600

    Adding in a meter bar for the percentage of votes each option has

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -11,113 +11,120 @@ $(function() {
 
   $.ajax({
     type: "GET",
-    url: "/api/poll/new",
-    data: JSON.stringify(newPoll),
+    url: "/api/poll/" + id,
     success: function(data) {
       console.log(data);
+      runPage(data);
     },
     contentType: "application/json"
   })
 
+  //bad
+  function runPage(poll) {
 
-  var pollStats;
+    var pollSocket = new WebSocket("ws://localhost:9000/ws/votes/" + id);
+    pollSocket.onmessage = function (event) {
+      var data    = JSON.parse(event.data);
+      var tallies = data.tallies;
+      var total   = data.total;
+      poll.tallies = tallies;
+      poll.total   = total;
+      var meters = $(".meter");
+      //console.log(meters);
+      $("div div div.optionCount span.tally").map(function(i, v) {
+        var percentage = Math.round(((tallies[i] / total) || 0) * 100);
+        console.log(percentage);
+        //console.log(meters[i]);
+        $(meters[i]).css("width", percentage + "%");
+        $(this).html("Votes: " + tallies[i] + " (" + percentage + "%)");
+        return tallies[i];
+      });
+    }
 
-  var pollSocket = new WebSocket("ws://localhost:9000/ws/votes/" + id);
-  pollSocket.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-    pollStats = data;
-    var tallies = data.tallies;
-    var total = data.total;
-    $("div div div.optionCount span.tally").map(function(i, v) {
-      var percentage = Math.round(((tallies[i] / total) || 0) * 100);
-      $(this).html("Votes: " + tallies[i] + " (" + percentage + "%)");
+    function combineData() {
+      //var tallies = $(".tally").map(function(i, e) {return $(e).text();});
+      var tallies = poll.tallies
+      //var options = $("pollBody div span.optionResult").map(function(i, e) {return $(e).text();});
+      var options = poll.options
+      var data = {};
+      for (i = 0; i < tallies.length; i++) {
+        data[options[i]] = tallies[i];
+      }
+      return data;
+    }
 
-      //$(this).html(tallies[i]);
-      return tallies[i];
-    });
+    var cv_w = 300,
+    cv_h = 300,
+    cv_r = 150
+    cv_color = d3.scale.category10();
+
+    var cv_arc = d3.svg.arc().outerRadius(cv_r);
+    var cv_pie = d3.layout.pie().sort(null).value(function (d) { return d.value });
+    var cv_svg = d3.select("div.mydiv")
+      .append("svg")
+      .attr("width", cv_w)
+      .attr("height", cv_h)
+      .attr("style", "display:block; margin: 0 auto;")
+      .append("g")
+      .attr("transform", "translate(" + cv_r + "," + cv_r + ")");
+
+    function cv_arcTween(a) {
+      var i = d3.interpolate(this._current, a);
+      this._current = i(0);
+      return function(t) {
+        return cv_arc(i(t));
+      };
+    }
+
+    function tests (data) {
+      data = data ? data : {
+        "slice1": Math.floor((Math.random()*10)+1),
+        "slice2": Math.floor((Math.random()*10)+1),
+        "slice3": Math.floor((Math.random()*10)+1),
+        "slice4": Math.floor((Math.random()*10)+1) };
+      var dataa = d3.entries(data);
+      var cv_path = cv_svg.selectAll("path").data(cv_pie(dataa));
+      var cv_text = cv_svg.selectAll("text").data(cv_pie(dataa));
+
+      cv_path.enter()
+        .append("path")
+        .attr("fill", function(d, i) { return cv_color(i); } )
+        .attr("d", cv_arc)
+        .each(function(d) { this._current = d; });
+      cv_text.enter()
+        .append("text")
+        .attr("transform", function(d) {
+          d.innerRadius = 0;
+          d.outerRadius = cv_r;
+          return "translate(" + cv_arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .attr("font-weight", "bold")
+        .attr("fill", "#FFFFFF")
+        .attr("font-size", "20px")
+
+      cv_text.text(function(d) {return d.data.key + "(" + d.data.value + ")";});
+
+      cv_path.transition().duration(750).attrTween("d", cv_arcTween);
+      cv_text.transition().duration(750).attr("transform", function(d) {
+        d.innerRadius = 0;
+        d.outerRadius = cv_r;
+        return "translate(" + cv_arc.centroid(d) + ")";
+      });
+
+      cv_path.exit().remove();
+      cv_text.exit().remove();
+    }
+
+    setInterval(function() {
+      var data = combineData();
+
+      var tallies = _.values(data);
+      var tallySum = _.reduce(tallies, function(memo, num){ return Number(memo) + Number(num); }, 0);
+
+      if (tallySum < 1) {console.log("failed"); return;}
+      tests(data);
+    }, 1000);
+
   }
 });
-
-function combineData() {
-  //var tallies = $(".tally").map(function(i, e) {return $(e).text();});
-  var tallies = pollStats.tallies;
-  var options = $("pollBody div span.optionResult").map(function(i, e) {return $(e).text();});
-  var data = {};
-  for (i = 0; i < tallies.length; i++) {
-    data[options[i]] = tallies[i];
-  }
-  return data;
-}
-
-var cv_w = 300,
-cv_h = 300,
-cv_r = 150
-cv_color = d3.scale.category10();
-
-var cv_arc = d3.svg.arc().outerRadius(cv_r);
-var cv_pie = d3.layout.pie().sort(null).value(function (d) { return d.value });
-var cv_svg = d3.select("div.mydiv")
-  .append("svg")
-  .attr("width", cv_w)
-  .attr("height", cv_h)
-  .attr("style", "display:block; margin: 0 auto;")
-  .append("g")
-  .attr("transform", "translate(" + cv_r + "," + cv_r + ")");
-
-function cv_arcTween(a) {
-  var i = d3.interpolate(this._current, a);
-  this._current = i(0);
-  return function(t) {
-    return cv_arc(i(t));
-  };
-}
-
-function tests (data) {
-  data = data ? data : {
-    "slice1": Math.floor((Math.random()*10)+1),
-    "slice2": Math.floor((Math.random()*10)+1),
-    "slice3": Math.floor((Math.random()*10)+1),
-    "slice4": Math.floor((Math.random()*10)+1) };
-  var dataa = d3.entries(data);
-  var cv_path = cv_svg.selectAll("path").data(cv_pie(dataa));
-  var cv_text = cv_svg.selectAll("text").data(cv_pie(dataa));
-
-  cv_path.enter()
-    .append("path")
-    .attr("fill", function(d, i) { return cv_color(i); } )
-    .attr("d", cv_arc)
-    .each(function(d) { this._current = d; });
-  cv_text.enter()
-    .append("text")
-    .attr("transform", function(d) {
-      d.innerRadius = 0;
-      d.outerRadius = cv_r;
-      return "translate(" + cv_arc.centroid(d) + ")";
-    })
-    .attr("text-anchor", "middle")
-    .attr("font-weight", "bold")
-    .attr("fill", "#FFFFFF")
-    .attr("font-size", "20px")
-
-  cv_text.text(function(d) {return d.data.key + "(" + d.data.value + ")";});
-
-  cv_path.transition().duration(750).attrTween("d", cv_arcTween);
-  cv_text.transition().duration(750).attr("transform", function(d) {
-    d.innerRadius = 0;
-    d.outerRadius = cv_r;
-    return "translate(" + cv_arc.centroid(d) + ")";
-  });
-
-  cv_path.exit().remove();
-  cv_text.exit().remove();
-}
-
-setInterval(function() {
-  var data = combineData();
-
-  var tallies = _.values(data);
-  var tallySum = _.reduce(tallies, function(memo, num){ return Number(memo) + Number(num); }, 0);
-
-  if (tallySum < 1) {console.log("failed"); return;}
-  tests(data);
-}, 1000);
