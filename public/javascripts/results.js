4a26097bbedeb1e6f823aab6fa6fae31fcc2e59b
tbrown1979 <tbrown@uni.edu>
2015-02-10 23:28:12 -0600

    Accidently grabbed the href, instead of hostname. Oops.

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -25,7 +25,7 @@ $(function() {
   }
 
   function watchForUpdates(poll) {
-    var pollSocket = new WebSocket("ws://" + window.location.href + "/ws/votes/" + id);
+    var pollSocket = new WebSocket("ws://" + window.location.hostname + "/ws/votes/" + id);
     pollSocket.onmessage = function (event) {
       var data    = JSON.parse(event.data);
       poll.tallies = data.tallies;
